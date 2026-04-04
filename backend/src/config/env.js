d072cb3946b49ca8e343dcd95e76a030df38318f
Arbaaz Murtaza <a.murtaza.29052@khi.iba.edu.pk>
2026-04-05 01:38:10 +0500

    Revert "chore(main): unify backend folder and CI workflow"
    This reverts commit d20123a758509b2c8c4e8fa187bc093ff1dd6d47.

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  databaseUrl: process.env.DATABASE_URL,
-};
--- backend/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const env = require('./config/env');
-
-app.listen(env.port, () => {
-  console.log(`Server running on port ${env.port}`);
-});
